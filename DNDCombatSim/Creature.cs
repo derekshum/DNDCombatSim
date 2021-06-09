4fdc8063b0d83363c525a31933e65030905804f2
derekshum <derekqlshum@gmail.com>
2021-06-09 12:35:28 -0400

    Adding roll string checking and processing.
    Use regex to check and string manipulation to process roll strings. Updated roll(int,int) to be die num and die size instead of roll bonus (bonuses handled explicitly now)

--- DNDCombatSim/Creature.cs
+++ DNDCombatSim/Creature.cs
@@ -9,20 +9,17 @@ namespace DNDCombatSim
 		public int currentHP;
 		public int armorClass;
 		public int attackBonus;
-		//TODO: public string damage;
-		public int damageDie;
-		public int damageBonus;
+		public string damage;
 		public int initiativeBonus;
 
-		public Creature(string nameIn, int maxHPIn, int armorClassIn, int attackBonusIn, int damageDieIn, int damageBonusIn, int initiativeBonusIn)
+		public Creature(string nameIn, int maxHPIn, int armorClassIn, int attackBonusIn, string damageIn, int initiativeBonusIn)
 		{
 			name = nameIn;
 			maxHP = maxHPIn;
 			currentHP = maxHP;
 			armorClass = armorClassIn;
 			attackBonus = attackBonusIn;
-			damageDie = damageDieIn;
-			damageBonus = damageBonusIn;
+			damage = damageIn;
 			initiativeBonus = initiativeBonusIn;
 		}
 	}
--- DNDCombatSim/Program.cs
+++ DNDCombatSim/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace DNDCombatSim
 {
@@ -6,16 +7,16 @@ namespace DNDCombatSim
     {
         static void Main(string[] args)
         {
-            Creature skeleton = new Creature("a skeleton", 13, 13, 4, 6, 2, 2);
-            Creature zombie = new Creature("a zombie", 22, 8, 3, 6, 1, -1);
+            Creature skeleton = new Creature("a skeleton", 13, 13, 4, "1d6+2", 2);
+            Creature zombie = new Creature("a zombie", 22, 8, 3, "1d6+1", -1);
 
             Battle1v1(skeleton, zombie);
         }
 
         static void Battle1v1(Creature creature1, Creature creature2)
         {
-            int initiative1 = roll(20, creature1.initiativeBonus);
-            int initiative2 = roll(20, creature2.initiativeBonus);
+            int initiative1 = roll(20) + creature1.initiativeBonus;
+            int initiative2 = roll(20) + creature2.initiativeBonus;
 
             Creature combatant1 = null;
             Creature combatant2 = null;
@@ -51,10 +52,10 @@ namespace DNDCombatSim
 
         static bool attack(Creature attacker, Creature target)
         {
-            int attackRoll = roll(20, attacker.attackBonus);
+            int attackRoll = roll(20) + attacker.attackBonus;
             if (attackRoll >= target.armorClass)
             {
-                int damage = roll(attacker.damageDie, attacker.damageDie);
+                int damage = roll(attacker.damage);
                 target.currentHP -= damage;
                 Console.WriteLine(attacker.name + " hits " + target.name + " with an attack dealing " + damage + " damage!");
                 if (target.currentHP <= 0)
@@ -72,17 +73,73 @@ namespace DNDCombatSim
 
         //TODO: add capitalization modification;
 
-        /*TODO
         static int roll(string rollString)
         {
-            //TODO: use regex?
-        }
-        */
+            int rollVal = 0;
+            
+            rollString = Regex.Replace(rollString.ToLower(), @"\s+", "");
 
-        static int roll(int die, int bonus)
+            //check that the roll string is in the correct format
+            Regex rollFormat = new Regex(@"^(\+|\-){0,1}\d*(d\d*){0,1}((\+|\-)\d*(d\d*){0,1})*$");
+            if (!rollFormat.IsMatch(rollString))
+            {
+                Console.WriteLine("Invalid Roll String!");
+                return 0;
+            }
+
+            string delimiters = @"([\+\-])";
+            string[] damageParts = Regex.Split(rollString, delimiters);
+
+            int i = 0;
+            while (i < damageParts.Length)
+            {
+                string currentPart = damageParts[i];
+                bool negative = false;
+                if (currentPart == "+" || currentPart == "-")
+                {
+                    if (currentPart == "-")
+                    {
+                        negative = true;
+                    }
+                    i++;
+                    currentPart = damageParts[i];
+                }
+
+                int changeVal = 0;
+                Regex rollUnitFormat = new Regex(@"^\d*d\d*$");
+                if (rollUnitFormat.IsMatch(currentPart))
+                {
+                    string[] rollParts = currentPart.Split('d');
+                    changeVal = roll(int.Parse(rollParts[0]), int.Parse(rollParts[1]));
+                }
+                else
+                {
+                    changeVal = int.Parse(currentPart);
+                }
+
+                if (negative)
+                    rollVal -= changeVal;
+                else
+                    rollVal += changeVal;
+
+                i++;
+            }
+
+            return rollVal;
+        }
+
+        static int roll(int numDie, int die)
+        {
+            int rollVal = 0;
+            for (int i = 0; i < numDie; i++)
+                rollVal += roll(die);
+            return rollVal;
+        }
+
+        static int roll(int die)
         {
             Random random = new System.Random();
-            return random.Next(1, die + 1) + bonus;
+            return random.Next(1, die + 1);
         }
     }
 }
